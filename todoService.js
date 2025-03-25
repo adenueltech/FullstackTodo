f72b91a9d5ef84b842a4ab19b5e501ecb91ec8c7
adenueltech <emmanueladewunmi51@gmail.com>
2025-03-25 21:26:57 +0100

    updated code

--- todoService.js
+++ todoService.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 require("dotenv").config();
 
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
-
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
